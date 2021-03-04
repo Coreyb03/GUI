5dc9a2c3c1054cb5e7a97a58df0c1a7984506173
Corey Becker <beckercn1@s.dcsdk12.org>
2021-03-04 11:09:45 -0700

    First Commit

---
+++ Calculator/src/App.java
@@ -0,0 +1,50 @@
+
+import java.awt.EventQueue;
+import java.util.*;
+
+import javax.swing.*;
+
+public class App extends JFrame{
+    public static void main(String[] args) throws Exception {
+        EventQueue.invokeLater(() -> {
+            App calc = new App();
+            calc.setVisible(true);
+        });
+    
+    }
+
+    public App() {
+        initUI();
+        
+    }
+
+    private void initUI(){
+        ArrayList<JComponent> buttonList = new ArrayList<JComponent>();
+
+        JButton button = new JButton("Quit");
+        button.addActionListener((Event) -> System.exit(0));
+        buttonList.add(button);
+
+        JButton button2 = new JButton("STOP");
+        button2.addActionListener((Event) -> System.out.println("HI"));
+        buttonList.add(button2);
+
+        createLayout(button, button2);
+
+        setTitle("Calaculator");
+        setSize(300,500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void createLayout(JComponent... args){
+        var pane = getContentPane();
+        var gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+        
+        gl.setHorizontalGroup(gl.createSequentialGroup().addComponent(args[0]).addComponent(args[1]));
+        gl.setVerticalGroup(gl.createParallelGroup().addComponent(args[0]).addComponent(args[1]));
+    }
+}
